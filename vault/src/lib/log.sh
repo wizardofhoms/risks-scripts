8a16d0b206518f7a04e2494ee5c5db34af7e89e2
wizardofhoms <wizardofhoms@proton.me>
2022-09-29 01:20:43 +0200

    Add logfile checks and output to logging code.

--- vault/src/lib/log.sh
+++ vault/src/lib/log.sh
@@ -57,10 +57,19 @@ function _msg()
 
 	[[ -n $_MSG_FD_OVERRIDE ]] && fd=$_MSG_FD_OVERRIDE
 
+    # If there is a log-file specified with flag --log-file,
+    # output the message to it, instead of the current file descriptor
+    logfile="${args[--log-file]}"
+    if [[ -n "${logfile}" ]]; then
+        ${=command} "${progname}" "${pchars}" "${msg}" >> "$logfile"
+        return $returncode
+    fi
+
+    # Else, print to stdout, with colors
 	if [[ -t $fd ]]; then
-	       [[ -n "$progname" ]] && progname="$fg[magenta]$progname$reset_color"
-	       [[ -n "$pchars" ]] && pchars="$fg_bold[$pcolor]$pchars$reset_color"
-	       msg="$fg[$pcolor]$msg$reset_color"
+       [[ -n "$progname" ]] && progname="$fg[magenta]$progname$reset_color"
+       [[ -n "$pchars" ]] && pchars="$fg_bold[$pcolor]$pchars$reset_color"
+       msg="$fg[$pcolor]$msg$reset_color"
 	fi
 
     ${=command} "${progname}" "${pchars}" "${msg}" >&"$fd"
@@ -90,14 +99,21 @@ function _warning() {
 	return 1
 }
 
-function _failure() {
+# failure first prints the message we have passed following the catch
+# of an error exit code, and then looks at the contents of erroring
+# command's stderr buffer, which is printed just below our message.
+# We then exit the program.
+function _failure() 
+{
     local section="$1"
     shift
     
 	typeset -i exitcode=${exitv:-1}
 
-    _msg failure "$section" "$@"               # First print the failure message info we passed
-    _message "$section" "$COMMAND_STDERR"   # And then print the command's failure message itself
+    _msg failure "$section" "$@"
+    if [[ -n "$COMMAND_STDERR" ]]; then
+        _msg inline "$section" "$COMMAND_STDERR"
+    fi
 
 	# Be sure we forget the secrets we were told
     exit "$exitcode"
@@ -113,54 +129,5 @@ function _failure() {
 function _print() {
     option_is_set -q || _msg print "$@"
 	return 0
-}
-
-# Variables
-
-COMMAND_STDOUT=''           # Stores a command's stdout output.
-COMMAND_STDERR=''           # Stores a command's stderr output.   
-
-# do a command, splitting and storing stdout/stderr output and printing
-# the former to screen only if the command is ran with verbose flag.
-# Returns the command's exit code, so we can catch any errors and inform.
-_run ()
-{
-    # First argument is the name of the section
-    local section="$1"
-    shift
-
-    # The STDOUT/STDERR variables are populated, which
-    # makes their content available to any subsequent call
-    # to _failure, which needs STDERR output
-    {
-        IFS=$'\n' read -r -d '' COMMAND_STDERR;
-        IFS=$'\n' read -r -d '' COMMAND_STDOUT;
-        (IFS=$'\n' read -r -d '' _ERRNO_; exit "${_ERRNO_}");
-    } < <((printf '\0%s\0%d\0' "$("$@")" "${?}" 1>&2) 2>&1)
-
-    local ret="$?"
-
-    # Output the command's result depending on the verbose mode
-    # and if the command ran successfully.
-    if [[ $ret -eq 0 ]] && is_verbose_set ; then
-        _verbose "$section" "$COMMAND_STDOUT"
-    fi
-
-    # Return the command's exit code
-    return $ret
-}
-
-# Checks the return code of a command, and if not successful,
-# fails with the associated error message. Usage:
-# catch $ret "hush" "Failed to execute this command"
-function _catch ()
-{
-    local ret="$?" 
-    local section="$1"
-    shift 
-
-    if [[ ! $ret -eq 0 ]]; then
-        _failure "$section" "$@"
-    fi
 }
 
