c3a43139cd7999e485e316513c3ab2d4a88fd6d0
wizardofhoms <wizardofhoms@proton.me>
2022-10-04 23:11:55 +0200

    Add verbose and some other flags to declarations

--- dom0/src/open_identity_command.sh
+++ dom0/src/open_identity_command.sh
@@ -1,4 +1,18 @@
-echo "# this file is located in 'src/open_identity_command.sh'"
-echo "# code for 'risk open identity' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+
+local identity="${args[identity]}"
+
+# 1 - Check that hush is mounted on vault
+# TODO: change this, since it only checks for the default vault VM
+check_is_device_attached "${SDCARD_BLOCK}" "${VAULT_VM}"
+
+# 2 - Check that no identity is currently opened
+# The second line should be empty, as opposed to being an encrypted coffin name
+check_no_active_identity "$identity"
+
+# 3 - Send commands to vault
+_message "Opening identity $identity"
+
+_qrun "$VAULT_VM" risks open identity "$identity"
+_catch "Failed to open identity"
+
+_message "Identity $identity is active"
