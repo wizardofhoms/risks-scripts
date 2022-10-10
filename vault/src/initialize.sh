7d189cb070457ed72a16e950a5f9aeb1c4475fa7
wizardofhoms <wizardofhoms@proton.me>
2022-10-10 18:40:40 +0200

    Add Makefile to vault risks and some fixes

--- vault/src/initialize.sh
+++ vault/src/initialize.sh
@@ -10,7 +10,30 @@ setopt +o nomatch
 # The generated script makes use of BASH_REMATCH, set compat for ZSH
 setopt BASH_REMATCH
 
+# Use colors unless told not to
+{ ! option_is_set --no-color } && { autoload -Uz colors && colors }
+
+## Checks ##
+
+# Don't run as root
+if [[ $EUID -eq 0 ]]; then
+   echo "This script must be run as user"
+   exit 2
+fi
+
+# Configuration file -------------------------------------------------------------------------------
+#
+# Directory where risk stores its state
+typeset -rg RISKS_DIR="${HOME}/.risks"  
+
+# Create the risk directory if needed
+[[ -e $RISKS_DIR ]] || { mkdir -p $RISKS_DIR && _message "Creating RISKS directory in $RISKS_DIR" }
+
+# Write the default configuration if it does not exist.
+config_init
+
 # Default filesystem settings from configuration file ----------------------------------------------
+
 typeset -gr SDCARD_ENC_PART="$(config_get SDCARD_ENC_PART)"
 typeset -gr SDCARD_ENC_PART_MAPPER="$(config_get SDCARD_ENC_PART_MAPPER)"
 typeset -gr SDCARD_QUIET="$(config_get SDCARD_QUIET)"
@@ -19,6 +42,7 @@ typeset -gr HUSH_DIR="$(config_get HUSH_DIR)"
 typeset -gr GRAVEYARD="$(config_get GRAVEYARD)"
 
 # Default tombs and corresponding mount points (CONSTANTS) .........................................
+
 typeset -gr GPG_TOMB_LABEL="GPG"          # Stores an identity GPG private keys. Seldom opened
 typeset -gr SSH_TOMB_LABEL="ssh"          # Stores SSH keypairs
 typeset -gr MGMT_TOMB_LABEL="mgmt"        # Holds the key-value store, and anything the user wants.
@@ -28,16 +52,17 @@ typeset -gr SIGNAL_TOMB_LABEL="signal"    # Holds data for Signal messenger (con
 typeset -gr FILE_ENCRYPTION="file_encryption_key" # Simply used as site name in spectre call.
 
 # Other default security-related default directories/names .........................................
-typeset -gr RAMDISK="${HOME}/.gnupg"      # Actually not a tomb mount point: used by coffin
+
+typeset -gr RAMDISK="${HOME}/.gnupg" 
 typeset -gr BACKUP_MOUNT_DIR="/tmp/pendrive"
 
-typeset -gr DEFAULT_KV_USER_DIR="$HOME/.tomb/mgmt/db/"   # Path to key=value store within mgmnt tomb 
-typeset -gr RISKS_SCRIPTS_INSTALL_PATH="${HUSH_DIR}/.risks-scripts" # Path to risks bin in the hush
+typeset -gr DEFAULT_KV_USER_DIR="$HOME/.tomb/mgmt/db/"
+typeset -gr RISKS_SCRIPTS_INSTALL_PATH="${HUSH_DIR}/.risks-scripts"
 
-typeset -rg RISKS_DIR="${HOME}/.risks"                         # Directory where risk stores its state
-typeset -rg RISKS_IDENTITY_FILE="${RISK_DIR}/.identity"        # Currently unlocked identity stored in file
+typeset -rg RISKS_IDENTITY_FILE="${RISK_DIR}/.identity"
 
 # Sensitive & and recurring variables used by program ..............................................
+
 typeset -gH IDENTITY
 typeset -gH FILE_ENCRYPTION_KEY
 typeset -gH GPG_PASS
@@ -50,14 +75,3 @@ export PASSWORD_STORE_ENABLE_EXTENSIONS=true
 export PASSWORD_STORE_GENERATED_LENGTH=20
 
 
-#----------------------------#
-## Checks ##
-
-# Don't run as root
-if [[ $EUID -eq 0 ]]; then
-   echo "This script must be run as user"
-   exit 2
-fi
-
-# Use colors unless told not to
-{ ! option_is_set --no-color } && { autoload -Uz colors && colors }
