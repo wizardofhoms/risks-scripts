613f0a8858fcfd5ad5c11948df5def1a4bdd5a90
wizardofhoms <wizardofhoms@proton.me>
2022-10-10 15:19:28 +0200

    Add configuration file and commands to vault risks - Remains to convert the code for using it correctly

---
+++ vault/src/config_set_command.sh
@@ -0,0 +1,7 @@
+
+local key value
+
+key="${args[key]}"
+value="${args[value]}"
+
+config_set "$key" "$value"
--- vault/src/initialize.sh
+++ vault/src/initialize.sh
@@ -7,6 +7,17 @@ export GPG_TTY
 # Remove verbose errors when * don't yield any match in ZSH
 setopt +o nomatch
 
+# The generated script makes use of BASH_REMATCH, set compat for ZSH
+setopt BASH_REMATCH
+
+# Default filesystem settings from configuration file ----------------------------------------------
+typeset -gr SDCARD_ENC_PART="$(config_get SDCARD_ENC_PART)"
+typeset -gr SDCARD_ENC_PART_MAPPER="$(config_get SDCARD_ENC_PART_MAPPER)"
+typeset -gr SDCARD_QUIET="$(config_get SDCARD_QUIET)"
+typeset -gr BACKUP_MAPPER="$(config_get BACKUP_MAPPER)"
+typeset -gr HUSH_DIR="$(config_get HUSH_DIR)"
+typeset -gr GRAVEYARD="$(config_get GRAVEYARD)"
+
 # Default tombs and corresponding mount points (CONSTANTS) .........................................
 typeset -gr GPG_TOMB_LABEL="GPG"          # Stores an identity GPG private keys. Seldom opened
 typeset -gr SSH_TOMB_LABEL="ssh"          # Stores SSH keypairs
@@ -17,12 +28,14 @@ typeset -gr SIGNAL_TOMB_LABEL="signal"    # Holds data for Signal messenger (con
 # Other default security-related default directories/names .........................................
 typeset -gr RAMDISK="${HOME}/.gnupg"      # Actually not a tomb mount point: used by coffin
 
-typeset -gr RISKS_IDENTITY_FILE="${HOME}/.identity"      # Currently unlocked identity stored in file
 typeset -gr DEFAULT_KV_USER_DIR="$HOME/.tomb/mgmt/db/"   # Path to key=value store within mgmnt tomb 
 typeset -gr RISKS_SCRIPTS_INSTALL_PATH="${HUSH_DIR}/.risks-scripts" # Path to risks bin in the hush
 typeset -gr BACKUP_MOUNT_DIR="/tmp/pendrive"
 
 typeset -gr FILE_ENCRYPTION="file_encryption_key" # Simply used as site name in spectre call.
+
+typeset -rg RISKS_DIR="${HOME}/.risks"                         # Directory where risk stores its state
+typeset -rg RISKS_IDENTITY_FILE="${RISK_DIR}/.identity"        # Currently unlocked identity stored in file
 
 # Sensitive & and recurring variables used by program ..............................................
 typeset -gH IDENTITY
